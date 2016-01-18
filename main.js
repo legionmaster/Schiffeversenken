88423a6a98471e35e52186f1a8a9b18051d1c970
Moritz <soldierhd123@gmail.com>
2016-01-18 12:22:36 +0100

    render Funktion erstellt und die wasser textur zugefügt.

--- main.js
+++ main.js
@@ -6,11 +6,39 @@ function createTable(table,x,y) {
     var tr = document.createElement("tr");
     for(var j=0; j < y; j++) {
       var td = document.createElement("td");
+      td.setAttribute("data-id", "0");
       tr.appendChild(td);
     }
     table.appendChild(tr);
   }
 }
 
-createTable(tablespieler, 10, 10);
-createTable(tablegegner, 10, 10);
+function render(table) {
+  var tds = table.getElementsByTagName('td');
+  for (var i = 0; i < tds.length; i++) {
+    var dataId = parseInt(tds[i].getAttribute("data-id"));
+    switch (dataId) {
+      case 0:
+        tds[i].setAttribute("class", "wasser");
+        break;
+      case 1:
+        tds[i].setAttribute("class", "schiff")
+        break;
+      case 2:
+        tds[i].setAttribute("class", "daneben")
+        break;
+      case 3:
+        tds[i].setAttribute("class", "getroffen")
+        break;
+      default:
+
+    }
+  }
+}
+function main() {
+  createTable(tablespieler, 10, 10);
+  createTable(tablegegner, 10, 10);
+  render(tablespieler);
+  render(tablegegner);
+}
+main();
